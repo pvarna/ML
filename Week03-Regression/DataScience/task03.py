5bd9f7297dea769f436c03b0692a09deb89211ef
petar.kolev <petar.kolev@ocado.com>
2025-10-25 16:53:50 +0300

    use RMSE instead of MSE

--- Week03-Regression/DataScience/task03.py
+++ Week03-Regression/DataScience/task03.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.metrics import r2_score, root_mean_squared_error
 from sklearn.linear_model import LinearRegression
 
 DATASET_PATH = os.path.join("..", "..", "DATA",
@@ -12,6 +12,7 @@ DATASET_PATH = os.path.join("..", "..", "DATA",
 TARGET = "sales"
 NON_NUMERICAL_FEATURES = ["influencer"]
 FEATURES_TO_IGNORE = ["influencer", "radio", "social_media"]
+# FEATURES_TO_IGNORE = ["influencer"]
 
 def r2_adjusted_score(samples_count, features_count, r2_score):
     return 1 - (1 - r2_score) * (samples_count - 1) / (samples_count - features_count - 1)
@@ -24,7 +25,7 @@ def main():
 
     X_train, X_test, y_train, y_test = train_test_split(X,
                                                         y,
-                                                        test_size=0.2,
+                                                        test_size=0.3,
                                                         random_state=21)
 
     samples_count, features_count = X_test.shape
@@ -34,45 +35,45 @@ def main():
 
     baseline_r2 = r2_score(y_test, y_pred_baseline)
     baseline_r2_adj = r2_adjusted_score(samples_count, features_count, baseline_r2)
-    baseline_mse = mean_squared_error(y_test, y_pred_baseline)
+    baseline_rmse = root_mean_squared_error(y_test, y_pred_baseline)
 
     print(f"Mean target value: {mean_y}")
     print(f"Baseline R^2: {baseline_r2:.4f}")
     print(f"Baseline R^2 adjusted: {baseline_r2_adj:.4f}")
-    print(f"Baseline MSE: {baseline_mse:.4f}")
+    print(f"Baseline RMSE: {baseline_rmse:.4f}")
     
 
-    linear_regression = LinearRegression()
+    linear_regression = LinearRegression(positive=True)
     linear_regression.fit(X_train, y_train)
 
     y_pred_linear_regression = linear_regression.predict(X_test)
     linear_regression_r2 = r2_score(y_test, y_pred_linear_regression)
     linear_regression_r2_adj = r2_adjusted_score(samples_count, features_count, linear_regression_r2)
-    linear_regression_r2_mse = mean_squared_error(y_test, y_pred_linear_regression)
+    linear_regression_r2_rmse = root_mean_squared_error(y_test, y_pred_linear_regression)
 
     print(f"Linear regression R^2: {linear_regression_r2:.4f}")
     print(f"Linear regression R^2 adjusted: {linear_regression_r2_adj:.4f}")
-    print(f"Linear regression MSE: {linear_regression_r2_mse:.4f}")
+    print(f"Linear regression RMSE: {linear_regression_r2_rmse:.4f}")
 
 
-    ax = sns.scatterplot(x=X["tv"], y=y)
-    sns.lineplot(x=X_test["tv"], y=y_pred_linear_regression, color="red", label="Prediction")
+    # ax = sns.scatterplot(x=X["tv"], y=y)
+    # sns.lineplot(x=X_test["tv"], y=y_pred_linear_regression, color="red", label="Prediction")
 
-    ax.set_title(f"Linear Regression")
-    ax.set_xlabel(f"TV Feature")
-    ax.set_ylabel(f"Sales Target")
-    plt.legend()
-    plt.tight_layout()
-    plt.show()
+    # ax.set_title(f"Linear Regression")
+    # ax.set_xlabel(f"TV Feature")
+    # ax.set_ylabel(f"Sales Target")
+    # plt.legend()
+    # plt.tight_layout()
+    # plt.show()
 
-    residuals = y_test - y_pred_linear_regression
-    sns.scatterplot(x=y_pred_linear_regression, y=residuals)
-    plt.axhline(0, color="red")
-    plt.title("Residuals vs Predicted Values")
-    plt.xlabel("Predicted Sales")
-    plt.ylabel("Residuals (Actual - Predicted)")
-    plt.tight_layout()
-    plt.show()
+    # residuals = y_test - y_pred_linear_regression
+    # sns.scatterplot(x=y_pred_linear_regression, y=residuals)
+    # plt.axhline(0, color="red")
+    # plt.title("Residuals vs Predicted Values")
+    # plt.xlabel("Predicted Sales")
+    # plt.ylabel("Residuals (Actual - Predicted)")
+    # plt.tight_layout()
+    # plt.show()
 
     # https://docs.google.com/spreadsheets/d/1FtbAG1QNvNbQBJJKCIoQrwFHvGgexOraszZJURoRaHY/edit?usp=sharing
 
